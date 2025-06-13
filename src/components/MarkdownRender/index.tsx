1ba0a5f8ac1e4b2127ac278861191dc3c7f83f29
vinky <liwenqi.v@bytedance.com>
2025-06-13 11:10:16 +0800

    feat: 引入highlight.js

--- src/components/MarkdownRender/index.tsx
+++ src/components/MarkdownRender/index.tsx
@@ -1,22 +1,36 @@
 import type { BubbleProps } from "@ant-design/x";
 
 import { Typography } from "antd";
+import hljs from "highlight.js";
 import markdownit from "markdown-it";
 import container from "markdown-it-container";
 import { Token } from "markdown-it/index.js";
 
 import "./index.scss";
+import "highlight.js/styles/github.css";
 
-const md = markdownit({ breaks: true, html: true }).use(container, "think", {
-  render(tokens: Token[], idx: number) {
-    const token = tokens[idx];
-    if (token.nesting === 1) {
-      return `<div class="think">\n`;
-    } else {
-      return `</div>\n`;
-    }
-  },
-});
+const md = markdownit({ breaks: true, html: true })
+  .use(container, "think", {
+    render(tokens: Token[], idx: number) {
+      const token = tokens[idx];
+      if (token.nesting === 1) {
+        return `<div class="think">\n`;
+      } else {
+        return `</div>\n`;
+      }
+    },
+  })
+  .use((md) => {
+    md.options.highlight = (str, lang) => {
+      if (lang && hljs.getLanguage(lang)) {
+        return `<pre class="hljs"><code>${
+          hljs.highlight(str, { language: lang }).value
+        }</code></pre>`;
+      }
+
+      return `<pre class="hljs"><code>${md.utils.escapeHtml(str)}</code></pre>`;
+    };
+  });
 
 const preprocessContent = (content: string) => {
   return content
